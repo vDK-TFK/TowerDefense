8c6804499f24332e3f521bb561729e65d98ca0bc
Daniela-Solano <128393379+Daniela-Solan@users.noreply.github.com>
2023-04-10 19:32:41 -0600

    Update MoverCpu.java

--- TowerDefense/src/MoverCpu.java
+++ TowerDefense/src/MoverCpu.java
@@ -7,6 +7,66 @@
  *
  * @author VDK
  */
-public class MoverCpu {
+public class MoverCpu extends Thread {
+    private ListaDobleCirc caminosuperior;
+    private ListaDobleCirc caminoinferior;
+    private Castillo jugadorCastillo;
+    private PantallaJuego pantalla;
+    private Enfrentamiento batalla = new Enfrentamiento();
+    //private MoverCpu moverCpu;
+
+    @Override
+    public void run() {
+        if (jugadorCastillo.castilloDestruido() != false) {
+            start();
+        }
+    }
+
+    public void moverCPU (ListaDobleCirc camino) {
+        for (int i = 0; i < camino.getLargo(); i++) {
+            if (camino.extrae(i).getTropa().getCaracter() != null) {
+
+                if (camino.isUltimo(i)) {
+                    jugadorCastillo.danoCastillo(camino.extrae(i).getTropa().getDanoCastillo());
+                    camino.extrae(i).setTropa(null);
+                    camino.extrae(i).eliminarTropa();
+
+                    if (jugadorCastillo.castilloDestruido()) {
+                        System.out.println("Ganador: CPU");
+                    }
+                } else if (camino.extrae(i - 1).getTropa() != null) {
+                    batalla.Enfrentamiento(camino.extrae(i - 1), camino.extrae(i));
+
+                    if (camino.extrae(i-1).getTropa() == null) {
+                        //poner la logica del logicaCpu
+                    } else {
+                        //poner la logica del cpu
+                    }
+
+                    if (camino.extrae(i).getTropa() != null) {
+                        camino.extrae(i - 1).setTropa(camino.extrae(i).getTropa());
+                        camino.extrae(i - 1).ingresarTropa();
+                        camino.extrae(i).setTropa(null);
+                        camino.extrae(i).eliminarTropa();
+                    } else {
+                        // parte de la pantalla
+                    }
+                } else {
+                    camino.extrae(i - 1).setTropa(camino.extrae(i).getTropa());
+                    camino.extrae(i - 1).ingresarTropa();
+                    camino.extrae(i).setTropa(null);
+                    camino.extrae(i).eliminarTropa();
+                }
+            }
+        }
+    }
+
+    public MoverCpu(ListaDobleCirc caminosuperior, ListaDobleCirc caminoinferior, Castillo jugadorCastillo, PantallaJuego pantalla) {
+        this.caminosuperior = caminosuperior;
+        this.caminoinferior = caminoinferior;
+        this.jugadorCastillo = jugadorCastillo;
+        this.pantalla = pantalla;
+    }
+    
     
 }
