d6a2a85b2bca2d4d0d904ca1f110a9bf497703d0
Daniela-Solano <128393379+Daniela-Solan@users.noreply.github.com>
2023-04-17 20:49:49 -0600

    Cronometro

---
+++ TowerDefense/src/Cronometro.java
@@ -0,0 +1,9 @@
+
+public class Cronometro {
+
+    public static void main(String[] args) {
+        PantallaJuego mFrmCronometro = new PantallaJuego();
+        mFrmCronometro.setVisible(true);
+    }
+    
+}
