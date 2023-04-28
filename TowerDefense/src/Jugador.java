5a97c649b8ca8c516b44d72a872889f25527ab65
andrewbocker <andrew.bocker26@gmail.com>
2023-04-27 23:41:46 -0600

    Reestructuracion de clases

--- TowerDefense/src/Jugador.java
+++ TowerDefense/src/Jugador.java
@@ -1,5 +1,3 @@
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,18 +8,5 @@
  * @author andre
  */
 public class Jugador {
-    
-    private Castillo castilloJugador;
-    private double numTropasCaminoSuperior = 0;
-    private double numTropasCaminoInferior = 0;
-    private double cantTropasTotales;
-    private Tropa tropaSeleccionada;
-    private Cola listaTropasJUG = new Cola();
-
-    public Jugador() {
-    }
-    
-    
-
     
 }
